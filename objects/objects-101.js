1432043233240fc6f5c82bfc39e1caf01e6dc0d5
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-12 02:50:48 +0500

    objects.

--- objects/objects-101.js
+++ objects/objects-101.js
@@ -1,3 +1,9 @@
-let myBook = {
-    
+let me = {
+    name: 'shaharyar',
+    age: 27,
+    location: 'karachi'
 }
+
+console.log(`${me.name} is ${me.age} & lives in ${me.location}.`)
+me.age = me.age -2
+console.log(`${me.name} is ${me.age} & lives in ${me.location}.`)
