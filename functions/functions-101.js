64f0a58f26281cfbe2121852e29427010891b0a8
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-11 22:45:00 +0500

    fuctuion-101

---
+++ functions/functions-101.js
@@ -0,0 +1,14 @@
+// funtion
+let greetUser = function () {
+    console.log('Welcome user')
+}
+
+greetUser()
+greetUser()
+greetUser()
+
+let sqr = function (nu) {
+    console.log(nu)
+}
+
+sqr(5)
