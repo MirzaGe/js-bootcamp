dc777210ab074f19702ee5f5ae45d42dfae9fd1b
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-11 23:43:52 +0500

    get tip calculator

---
+++ functions/arguments.js
@@ -0,0 +1,17 @@
+let add = function (a, b) {
+    return a+b
+
+
+}
+
+let result = add(10,2)
+console.log(result)
+
+
+
+let scoreTy = function (name,score){
+return 'Name:' + name + ' --Goals : ' + score
+}
+
+let scoreTe = scoreTy('undefined',6)
+console.log(scoreTe)
