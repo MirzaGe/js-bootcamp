41147317908f8c26801f653b4b541af7d14940b8
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-12 21:20:18 +0500

    making new file cont.js

--- objects/number-methods.js
+++ objects/number-methods.js
@@ -1,3 +1,10 @@
 let num = 103.941
 
 console.log(num.toFixed(1))
+console.log(Math.round(num))
+
+
+let min = 0
+let max = 1
+let randomNum = Math.floor((Math.random() * (max - min +1))) + min
+console.log(randomNum)
