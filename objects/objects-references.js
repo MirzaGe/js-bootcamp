ee7e919a407fd50648ada4f4c44c15ccc4f59950
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-12 17:11:59 +0500

    expenses

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -1,3 +1,14 @@
 let myAccount = {
-    
+    name:'Shaharyar Baig',
+    expenses:150,
+    income:0
 }
+
+let addExpense = function (account,amount) {
+    account.expenses = account.expenses + amount
+console.log(account)
+}
+
+addExpense(myAccount, 137)
+console.log(myAccount)
+    
