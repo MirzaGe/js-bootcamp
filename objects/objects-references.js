46262a4cbf8c1d776af3c27fa32417d5cf0b66d5
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-12 18:35:37 +0500

    account managing

--- objects/objects-references.js
+++ objects/objects-references.js
@@ -6,9 +6,25 @@ let myAccount = {
 
 let addExpense = function (account,amount) {
     account.expenses = account.expenses + amount
-console.log(account)
 }
 
-addExpense(myAccount, 137)
-console.log(myAccount)
+let addIncome = function (account,income) {
+    account.income = account.income + income
+}
+
+let resetAccount = function (account) {
+    account.expenses = 0
+    account.income = 0
+}
+
+    let getAccountSummary = function (account) {
+        let balance = account.income - account.expenses
+        return `Account for ${account.name} has $${account.income}
+         in income. $${account.expenses}`
+    }
+
+    addIncome(myAccount,2000)
+    addExpense(myAccount,2.50)
+    addExpense(myAccount,160)
+    console.log(getAccountSummary(myAccount))
     
