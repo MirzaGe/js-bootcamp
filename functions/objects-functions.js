1e8094f06addd582092a086651d46c670d55d4e2
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-12 16:59:41 +0500

    chages

--- functions/objects-functions.js
+++ functions/objects-functions.js
@@ -1,18 +1,24 @@
 let myBook = {
-    title: 1984 ,
-    author:'George Orwell'
-    pageCount : 326
+    title: '1984' ,
+    author:'George Orwell',
+    pageCount: 326, 
 }
 
 let otherBook = {
     tittle: 'A people History',
-    author: 'Howard Zinn'
-    pageCount: 723
+    author: 'Howard Zinn',
+    pageCount: 723 ,
 }
 
 let getSummary = function (book) {
-    console.log(`${book.title} by ${book.author}`)
+    return {
+summary:`${book.title} is ${book.pageCount}`,
+pageCountSummary: `${book.title} is ${book.pageCount}pages long`
+    }
 }
 
-getSummary(myBook)
-getSummary(otherBook)
+let bookSummary=getSummary(myBook)
+let otherBookSummary=getSummary(otherBook)
+
+console.log(bookSummary.pageCountSummary)
+
