211d62295b050243113c63c5266df15c4451a518
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-13 00:03:42 +0500

    todo assignment

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+// const todos = ['order cat food','clean','Buy Food','Do work','Exercise']
+// console.log(`You have ${todos.length} todos!`)
+// console.log(`Todo:: ${todos[0]}`)
+// console.log(`Todo: ${todos[todos.length-2]}`)
+
