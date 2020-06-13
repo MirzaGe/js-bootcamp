56bc185f9f842ff5c9d317c45ab1ca17e2ea91da
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-13 17:18:06 +0500

    for loop

--- arrays/notes.js
+++ arrays/notes.js
@@ -1,4 +1,13 @@
 const notes = ['Note 1','Note 2','Note5']
 
-console.log(notes.length)
-console.log(notes)
+// notes.forEach(function(nm){
+//     console.log(nm)
+// })
+
+// console.log(notes.length)
+// console.log(notes)
+
+for(let count =0; count < notes.length;
+    count++){
+        console.log(notes[count])
+    }
