2968e19541bb65012bcb4d91ad36b491f9833886
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-13 17:28:06 +0500

    update

--- arrays/todo.js
+++ arrays/todo.js
@@ -1,5 +1,11 @@
-// const todos = ['order cat food','clean','Buy Food','Do work','Exercise']
 // console.log(`You have ${todos.length} todos!`)
 // console.log(`Todo:: ${todos[0]}`)
 // console.log(`Todo: ${todos[todos.length-2]}`)
+
+ const todos = ['order cat food','clean','Buy Food','Do work','Exercise']
+ for (let count = 0;todos.length<count;count++){
+     const num = count +1
+     const todo = todos[count]
+     console.log(`${num}. ${todo}`)
+ }
 
