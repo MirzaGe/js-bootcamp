2799c710684da0f1769060b3a190e900e01ab09b
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-16 21:26:59 +0500

    update

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,7 +1,32 @@
-const paragraphs = document.querySelectorAll('p')
+const todos= [{
+    text: 'Order cat food',
+    completed: false
+},{
+    text: 'clean kitchen',
+    completed: true
+}, {
+    text:'Buy food',
+    completed: true
+}, {
+    text: 'Do work',
+    completed:false
+}, {
+    text: 'Exercise',
+    completed: true
+}]
 
-paragraphs.forEach(function (paragraph) {
-    if (paragraph.textContent.includes('the')){
-        paragraph.remove()
-    }
+const incompleteTodos= todos.filter(function (todo){
+    return !todo.completed
 })
+
+const summary = document.createElement('h2')
+summary.textContent = `You have ${incompleteTodos.length} todos left`
+document.querySelector('body').appendChild(summary)
+
+todos.forEach(function (todo){
+    const p = document.createElement('p')
+    p.textContent = todo.text
+    document.querySelector('body').appendChild(p)
+
+})
+
