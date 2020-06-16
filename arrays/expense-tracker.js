89c7753d6b25c57ba0b01b7404c46c7f78086fc0
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-16 09:36:26 +0500

    expenses tracker

--- arrays/expense-tracker.js
+++ arrays/expense-tracker.js
@@ -1,5 +1,19 @@
 const account = {
     name: 'shaharyar',
-    expenses:[]
+    expenses:[],
+    addExpense: function (description,amount){
+        this.expenses.push({
+            description: description,
+            amount: amount
+        })
+    },
+    getAccountSummary: function () {
+        let totalExpenses = 0
+        return `${this.name} has $${totalExpenses} in expenses.`
+
+    }
 }
 
+ account.addExpense('Rent',950)
+ account.addExpense('Coffe',2)
+ console.log(account.getAccountSummary())
