c06eedf439ac9a858158fe5a55a873d560d4f4d4
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-16 20:44:34 +0500

    update

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -1,6 +1,16 @@
+// const ps = document.querySelectorAll('p')
+
+// ps.forEach(function (p) {
+//       p.textContent = '******'
+//     // p.remove()
+// })
+
 const ps = document.querySelectorAll('p')
 
-ps.forEach(function (p) {
-      p.textContent = '******'
-    // p.remove()
+ps.forEach(function(p){
+  p.textContent = '****'
 })
+
+const newParagraph = document.createElement('p')
+newParagraph.textContent = 'This is a new element from Javascript'
+document.querrySelector('body').appendChild(newParagraph)
