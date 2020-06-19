e69a68f6f936a8162ce97e941fc3495854379e9b
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-19 23:45:06 +0500

    commit

--- functions/functions-101.js
+++ functions/functions-101.js
@@ -1,27 +1,27 @@
-/*// funtion
-let greetUser = function () {
-    console.log('Welcome user')
-}
+// /*// funtion
+// let greetUser = function () {
+//     console.log('Welcome user')
+// }
 
-greetUser()
-greetUser()
-greetUser()
+// greetUser()
+// greetUser()
+// greetUser()
 
-let sqr = function (num) {
-    console.log(num)
-}
+// let sqr = function (num) {
+//     console.log(num)
+// }
 
-sqr(5)
-*/
+// sqr(5)
+// */
 
-let convertFarenheitToCelcius = function(fahrenheit) {
-let celsius = (fahrenheit - 32)* 5/9
-return celsius
-}
+// let convertFarenheitToCelcius = function(fahrenheit) {
+// let celsius = (fahrenheit - 32)* 5/9
+// return celsius
+// }
 
-let tempOne = convertFarenheitToCelcius(32)
-let tempTwo = convertFarenheitToCelcius(69)
+// let tempOne = convertFarenheitToCelcius(32)
+// let tempTwo = convertFarenheitToCelcius(69)
 
-console.log(tempOne)
-console.log(tempTwo)
+// console.log(tempOne)
+// console.log(tempTwo)
 
