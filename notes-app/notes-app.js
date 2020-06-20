a57eb9c461e4b6ee24cbdd83459dce539ecdb604
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-20 05:38:45 +0500

    notes-app

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -7,10 +7,3 @@
 
 const ps = document.querySelectorAll('p')
 
-ps.forEach(function(p){
-  p.textContent = '****'
-})
-
-const newParagraph = document.createElement('p')
-newParagraph.textContent = 'This is a new element from Javascript'
-document.querrySelector('body').appendChild(newParagraph)
