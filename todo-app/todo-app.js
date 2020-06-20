a702884331445a5a0a28f999b8f5a28a81825e9a
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-20 07:02:10 +0500

    get/

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -19,14 +19,14 @@ const incompleteTodos= todos.filter(function (todo){
     return !todo.completed
 })
 
-const summary = document.createElement('h2')
+const summary = document.createElement('h4')
 summary.textContent = `You have ${incompleteTodos.length} todos left`
 document.querySelector('body').appendChild(summary)
 
 todos.forEach(function (todo){
-    const p = document.createElement('p')
-    p.textContent = todo.text
-    document.querySelector('body').appendChild(p)
+    const para = document.createElement('p')
+    para.textContent = todo.text
+    document.querySelector('body').appendChild(para)
 
 })
 
