66ecb81c7fc1e0ae10dde29e325b9f4699d8752b
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-21 23:36:49 +0500

    removing event handler

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -33,10 +33,7 @@ document.querySelector('#create-note').addEventListener('click', function (e) {
   e.target.textContent = 'The button was clicked'
 })
 
-document.querySelector('#remove-all').addEventListener('click', function () {
-  document.querySelectorAll('.note').forEach(function( note) {
-      note.remove()
-  })
+d
 })
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
