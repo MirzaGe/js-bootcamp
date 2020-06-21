68bf37405f4f7a8c1e4629b5438750201ee70143
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-21 23:29:13 +0500

    copied todo.js from notepad

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,74 +1,59 @@
-const todos= [{
+const todos = [{
     text: 'Order cat food',
     completed: false
-},{
-    text: 'clean kitchen',
+}, {
+    text: 'Clean kitchen',
     completed: true
 }, {
-    text:'Buy food',
+    text: 'Buy food',
     completed: true
 }, {
     text: 'Do work',
-    completed:false
+    completed: false
 }, {
     text: 'Exercise',
     completed: true
 }]
 
 const filters = {
-    searchText:''
+    searchText: ''
 }
 
+const renderTodos = function (todos, filters) {
+    const filteredTodos = todos.filter(function (todo) {
+        return todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
+    })
 
+    const incompleteTodos = filteredTodos.filter(function (todo) {
+        return !todo.completed
+    })
 
-const incompleteTodos= todos.filter(function (todo){
-    return !todo.completed
+    document.querySelector('#todos').innerHTML = ''
+
+    const summary = document.createElement('h2')
+    summary.textContent = `You have ${incompleteTodos.length} todos left`
+    document.querySelector('#todos').appendChild(summary)
+
+    filteredTodos.forEach(function (todo) {
+        const p = document.createElement('p')
+        p.textContent = todo.text
+        document.querySelector('#todos').appendChild(p)
+    })
+}
+
+renderTodos(todos, filters)
+
+// Listen for new todo creation
+document.querySelector('#add-todo').addEventListener('click', function (e) {
+    console.log('Add a new todo...')
 })
 
-const summary = document.createElement('h4')
-summary.textContent = `You have ${incompleteTodos.length} todos left`
-document.querySelector('body').appendChild(summary)
-
-todos.forEach(function (todo){
-    const para = document.createElement('p')
-    para.textContent = todo.text
-    document.querySelector('body').appendChild(para)
-
+// Listen for todo text change
+document.querySelector('#new-todo-text').addEventListener('input', function (e) {
+    console.log(e.target.value)
 })
 
-document.querySelector('#add-todo').addEventListener
-('click',function(e){
-    console.log('Add a new Todo...')
-})
-
-document.querySelector('#new-todo-text').addEventListener('input', function(e) {
-console.log(e.target.value)
-
-})
-
-document.querySelector('#search-text').addEventListener('input',function(e){
+document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
+    renderTodos(todos, filters)
 })
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
