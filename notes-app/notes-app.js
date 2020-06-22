54259a535bc3040588912b6929d7d893a7dc1985
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-22 05:30:01 +0500

    60 drop-down

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -38,6 +38,6 @@ document.querySelector('#search-text').addEventListener('input', function (e) {
   renderNotes(notes, filters)
 })
 
-document.querySelector('#for-fun').addEventListener('change', function (e) {
-  console.log(e.target.checked)
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+  console.log(e.target.value)
 })
