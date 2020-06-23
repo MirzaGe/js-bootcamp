b1566ad7b77f1c66e04d15bba8b956d6456d1445
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-23 11:52:30 +0500

    todo app local storage

--- todo-app/todo-app.js
+++ todo-app/todo-app.js
@@ -1,73 +1,52 @@
-const todos = [{
-    text: 'Order cat food',
-    completed: false
-}, {
-    text: 'Clean kitchen',
-    completed: true
-}, {
-    text: 'Buy food',
-    completed: true
-}, {
-    text: 'Do work',
-    completed: false
-}, {
-    text: 'Exercise',
-    completed: true
-}]
+let notes = []
 
 const filters = {
-    searchText: '',
-    hideCompleted: false
+    searchText: ''
 }
 
-const renderTodos = function (todos, filters) {
-    const filteredTodos = todos.filter(function (todo) {
-        const searchTextMatch = todo.text.toLowerCase().includes(filters.searchText.toLowerCase())
-        const hideCompletedMatch = !filters.hideCompleted || !todo.completed
+// Check for existing saved data
+const notesJSON = localStorage.getItem('notes')
 
-        return searchTextMatch && hideCompletedMatch
+if (notesJSON !== null) {
+    notes = JSON.parse(notesJSON)
+}
+
+const renderNotes = function (notes, filters) {
+    const filteredNotes = notes.filter(function (note) {
+        return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
     })
 
-    const incompleteTodos = filteredTodos.filter(function (todo) {
-        return !todo.completed
-    })
+    document.querySelector('#notes').innerHTML = ''
+    
+    filteredNotes.forEach(function (note) {
+        const noteEl = document.createElement('p')
 
-    document.querySelector('#todos').innerHTML = ''
-
-    const summary = document.createElement('h2')
-    summary.textContent = `You have ${incompleteTodos.length} todos left`
-    document.querySelector('#todos').appendChild(summary)
-
-    filteredTodos.forEach(function (todo) {
-        const p = document.createElement('p')
-        p.textContent = todo.text
-        document.querySelector('#todos').appendChild(p)
+        if (note.title.length > 0) {
+            noteEl.textContent = note.title
+        } else {
+            noteEl.textContent = 'Unnamed note'
+        }
+        
+        document.querySelector('#notes').appendChild(noteEl)
     })
 }
 
-renderTodos(todos, filters)
+renderNotes(notes, filters)
+
+document.querySelector('#create-note').addEventListener('click', function (e) {
+    notes.push({
+        title: '',
+        body: ''
+    })
+    localStorage.setItem('notes', JSON.stringify(notes))
+    renderNotes(notes, filters)
+})
 
 document.querySelector('#search-text').addEventListener('input', function (e) {
     filters.searchText = e.target.value
-    renderTodos(todos, filters)
+    renderNotes(notes, filters)
 })
 
-document.querySelector('#new-todo').addEventListener('submit', function (e) {
-    e.preventDefault()
-    todos.push({
-        text: e.target.elements.text.value,
-        completed: false
-    })
-    renderTodos(todos, filters)
-    e.target.elements.text.value = ''
+document.querySelector('#filter-by').addEventListener('change', function (e) {
+    console.log(e.target.value)
 })
-
-document.querySelector('#hide-completed').addEventListener('change', function (e) {
-    filters.hideCompleted = e.target.checked
-    renderTodos(todos, filters)
-})
-
-// 1. Create a checkbox and setup event listener -> "Hide completed"
-// 2. Create new hideCompleted filter (default false)
-// 3. Update hideCompleted an rerender list on checkbox change
-// 4. Setup renderTodos to remove completed items
