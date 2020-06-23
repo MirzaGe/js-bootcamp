317518a18e391d8c61a54b89b15cfd5c43a0016a
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-23 10:59:14 +0500

    storage

--- notes-app/notes-app.js
+++ notes-app/notes-app.js
@@ -13,6 +13,18 @@ const filters = {
   searchText: ''
 }
 
+// const user = {
+//     name: 'Andrew',
+//     age: 27
+// }
+// const userJSON = JSON.stringify(user)
+// console.log(userJSON)
+// localStorage.setItem('user', userJSON)
+
+const userJSON = localStorage.getItem('user')
+const user = JSON.parse(userJSON)
+console.log(`${user.name} is ${user.age}`)
+
 const renderNotes = function (notes, filters) {
   const filteredNotes = notes.filter(function (note) {
       return note.title.toLowerCase().includes(filters.searchText.toLowerCase())
