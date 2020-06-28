c52a0f67490ff59299308ffe43c458c36900ea08
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-06-28 20:24:35 +0500

    update

--- advance/arrow-function.js
+++ advance/arrow-function.js
@@ -1,23 +1,23 @@
-const square = (num) => {
-    return num * num
-}
-console.log(square(5))
+// const square = (num) => {
+//     return num * num
+// }
+// console.log(square(5))
 
-const people = [{
-    name: 'Andrew',
-    age:27
-}, {
-    name:'Vikram',
-    age:31
-}, {
-    name: 'jess',
-    age:22
-}]
+// const people = [{
+//     name: 'Andrew',
+//     age:27
+// }, {
+//     name:'Vikram',
+//     age:31
+// }, {
+//     name: 'jess',
+//     age:22
+// }]
 
-const under30 = people.filter(function(person){
-return person.age < 30
+// const under30 = people.filter(function(person){
+// return person.age < 30
 
-})
-console.log(under30) 
+// })
+// console.log(under30) 
 
-const under22= people.filter((person) => person.age <= 22 )
+// const under22= people.filter((person) => person.age <= 22 )
