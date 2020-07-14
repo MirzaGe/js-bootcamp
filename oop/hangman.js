6718b4156a941c660aae9d0a24229658680e6a7d
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-07-15 00:02:32 +0500

    hangan.js

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,10 +1,25 @@
 const Hangman = function (word, remainingGuesses) {
-    this.word = word
+    this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
+    this.guessedLetters = ['c', 'e']
+}
+
+Hangman.prototype.getPuzzle = function () {
+    let puzzle = ''
+
+    this.word.forEach((letter) => {
+        if (this.guessedLetters.includes(letter) || letter === ' ') {
+            puzzle += letter
+        } else {
+            puzzle += '*'
+        }
+    })
+
+    return puzzle
 }
 
 const game1 = new Hangman('Cat', 2)
-console.log(game1)
+console.log(game1.getPuzzle())
 
 const game2 = new Hangman('New Jersey', 4)
-console.log(game2)
+console.log(game2.getPuzzle())
