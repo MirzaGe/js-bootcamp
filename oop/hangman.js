6c14f43e6ff993437ed1fbd9e76a53a66280845b
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-07-15 00:18:49 +0500

    t,w

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,7 +1,7 @@
 const Hangman = function (word, remainingGuesses) {
     this.word = word.toLowerCase().split('')
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c', 'e']
+    this.guessedLetters = ['t', 'w']
 }
 
 Hangman.prototype.getPuzzle = function () {
