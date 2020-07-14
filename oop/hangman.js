63127bf6944c18dfa1163e5b90ae861320e6d63a
Mirza Shaharyar Baig <shareyscorpi1@gmail.com>
2020-07-15 00:11:30 +0500

    get puzzle

--- oop/hangman.js
+++ oop/hangman.js
@@ -1,25 +1,10 @@
-const Hangman = function (word, remainingGuesses) {
-    this.word = word.toLowerCase().split('')
+const Hangman = function(word, remainingGuesses) {
+    this.word = word.toLowerCase()
     this.remainingGuesses = remainingGuesses
-    this.guessedLetters = ['c', 'e']
 }
 
-Hangman.prototype.getPuzzle = function () {
-    let puzzle = ''
-
-    this.word.forEach((letter) => {
-        if (this.guessedLetters.includes(letter) || letter === ' ') {
-            puzzle += letter
-        } else {
-            puzzle += '*'
-        }
-    })
-
-    return puzzle
-}
-
-const game1 = new Hangman('Cat', 2)
+const game1 = new Hangman('cat',2)
 console.log(game1.getPuzzle())
 
-const game2 = new Hangman('New Jersey', 4)
-console.log(game2.getPuzzle())
+const game2 = new Hangman('New jersey',4)
+console.log(game2.getpuzzle())
